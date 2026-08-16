a1a7d67994f9845dacfd56b026e3c31ccae25a80
solvise <solvise18@gmail.com>
2026-08-16 15:13:00 +0000

    add guess game

--- src/main/java/com/mycompany/javatest/Javatest.java
+++ src/main/java/com/mycompany/javatest/Javatest.java
@@ -1,14 +1,29 @@
 package com.mycompany.javatest;
 
 import java.util.Scanner;
+import java.util.Random;
 
 public class Javatest {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
+        Random random = new Random();
         
-        System.out.println("What is your name");
-        String name = scanner.nextLine();
+        int target = random.nextInt(100) + 1;
+        int guess = 0;
+        int attempts = 0;
         
-        System.out.println("Hello, " + name + "!");
+        while (guess != target) {
+            System.out.println("Guess number beetween 1 and 100:");
+            guess = scanner.nextInt();
+            attempts = attempts +1;
+            
+            if (guess < target){
+                System.out.println("Too low!");
+                } else if (guess > target) {
+                    System.out.println("Too high!");
+                }
+        }
+       System.out.println("Correct. The number was " + target);
+       System.out.println("It took " + attempts + "attempts to guess the number.");
     }
 }
